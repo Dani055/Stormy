00126aabd33008e5b9629315ced6ec2f4924c2e7
Nikoleta Dimitrova <n.dimitrova@student.fontys.nl>
2023-04-05 10:22:29 +0200

    Added more safe spots

--- StormyClient/StormyClient/model/MapLocations.swift
+++ StormyClient/StormyClient/model/MapLocations.swift
@@ -26,6 +26,7 @@ let MapLocations = [
     MapLocation(name: "Fontys", street: "Rachelsmolen 1,", mw: 15, latitude: 51.451280085729536, longitude: 5.479830842508337),
     MapLocation(name: "De bijenkorf", street: "Piazza 1", mw: 10, latitude: 51.441768123088714, longitude: 5.477381885404868),
     MapLocation(name: "TU/e library",street: "De Groene Loper 5", mw: 20,latitude: 51.44868332980911, longitude: 5.490714798327613),
-    MapLocation(name: "Tea Stories",street: "Leidingstraat 203", mw: 4, latitude: 51.44866014136003, longitude: 5.455672083859242),
-    MapLocation(name: "AH Torenalee",street: "Torenallee 86", mw: 2,latitude: 51.44817586277018, longitude: 5.453795000062569)
+    MapLocation(name: "Underpass near Strijp-S",street: "Marconilaan", mw: 1, latitude: 51.450031, longitude: 5.457477),
+    MapLocation(name: "AH Torenalee",street: "Torenallee 86", mw: 3,latitude: 51.44817586277018, longitude: 5.453795000062569),
+    MapLocation(name: "Underpass railway",street: "Bezuidenhoutseweg 22", mw: 4,latitude: 51.456413, longitude: 5.448896)
 ]
